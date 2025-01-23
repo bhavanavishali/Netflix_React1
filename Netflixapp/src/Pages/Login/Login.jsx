b91d1d93abe2228285b3fd35a49b32c68893a5a8
bhavanavishali <bhavanakukku894@gmail.com>
2025-01-23 17:00:24 +0530

    Create Sign In and sign UP Page of Netflix

--- Netflixapp/src/Pages/Login/Login.jsx
+++ Netflixapp/src/Pages/Login/Login.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
 import  './Login.css'
+import { useState } from 'react'
+import logo from '../../assets/logo.png'
+
 
 function Login() {
+  const [signState,setSignstate]=useState("Sign In")
+
   return (
-    <div className='login'></div>
+    <div className='login'>
+      <img src={logo} className="login-logo" alt="" />
+      <div className="login-form">
+          <h1>{signState}</h1>
+          <form>
+            {signState ==="Sign Up" ?  <input type="text" placeholder='Your name'/> :<></>}  {/* // only display your name feild in Sign Up time o */}
+                                                                                
+           
+            <input type="email" placeholder='Email' />
+            <input type="password" placeholder='Password' />
+            <button>{signState}</button>
+            <div className="form-help">
+              <div className="remember">
+              <input type="checkbox" />
+              <label htmlFor="">Remember me!</label>
+              </div>
+              <p>Need Help?</p>
+            </div>
+
+          </form>
+          <div className="form-switch">
+            {signState ==="Sign In" ? 
+            <p>New to Netflix? <span onClick={()=>{setSignstate("Sign Up")}}>Sign Up Now</span></p>
+            :<p>Already have an Acount <span onClick={()=>{setSignstate("Sign In")}}>Sign In Now</span></p>
+            }
+            
+            
+          </div>
+      </div>
+    </div>
   )
 }
 
